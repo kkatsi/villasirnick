3526c5874b6b4a85ec1e8a8611bfd9369cb13ecc
Kostas <kostas.katsinaris@weare8.com>
2024-12-25 19:41:40 +0200

    chore: env vars

--- app/config.ts
+++ app/config.ts
@@ -1,28 +1,13 @@
-export const config =
-  typeof window !== 'undefined'
-    ? {
-        mailSender: {
-          host: '',
-          port: '',
-          user: '',
-          password: '',
-        },
-        mailReceiverAddress: '',
-        captcha: {
-          secretKey: '',
-          siteKey: import.meta.env.VITE_CAPTCHA_SITE_KEY,
-        },
-      }
-    : {
-        mailSender: {
-          host: process.env.MAIL_HOST,
-          port: process.env.MAIL_PORT,
-          user: process.env.MAIL_USER,
-          password: process.env.MAIL_PASSWORD,
-        },
-        mailReceiverAddress: process.env.MAIL_RECEIVER_ADDRESS,
-        captcha: {
-          secretKey: process.env.CAPTCH_SECRET_KEY,
-          siteKey: process.env.CAPTCHA_SITE_KEY,
-        },
-      };
+export const config = {
+  mailSender: {
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    user: process.env.MAIL_USER,
+    password: process.env.MAIL_PASSWORD,
+  },
+  mailReceiverAddress: process.env.MAIL_RECEIVER_ADDRESS,
+  captcha: {
+    secretKey: process.env.CAPTCH_SECRET_KEY,
+    siteKey: process.env.CAPTCHA_SITE_KEY,
+  },
+};
--- app/root.tsx
+++ app/root.tsx
@@ -13,6 +13,7 @@ import type { Route } from './+types/root';
 import stylesheet from './app.css?url';
 
 import { GoogleReCaptchaProvider } from '@google-recaptcha/react';
+import { config } from './config';
 
 export const links: Route.LinksFunction = () => [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -48,10 +49,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return (
-    <GoogleReCaptchaProvider
-      type="v2-checkbox"
-      siteKey={'6LePQ6QqAAAAAHZMfWftxqKXAb42egmwbXK_AWfg'}
-    >
+    <GoogleReCaptchaProvider type="v2-checkbox" siteKey={config.captcha.siteKey ?? ''}>
       <Outlet />
       <Toaster />
     </GoogleReCaptchaProvider>
